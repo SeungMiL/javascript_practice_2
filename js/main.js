7cf969f0dc6bfdc3c7d4d77218036031c5f2c8fc
SeungMiL <tmskqj@naver.com>
2021-12-10 23:47:51 +0900

    javascript_practice_history3

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 //+ 버튼을 클릭하면, 할일이 추가된다
 // delete버튼을 누르면 할일이 삭제된다
 // check버튼을 누르면 할일이 끝나면서 밑줄이 간다
+//1. check  버튼을 클릭하는 순간 true false
+//2. true이면 안끝난
+
 // 진행중 끝남 탭을 누르면, 언더바가 이동한다
 // 끝남탭은, 끝난 아이템만, 진행중탭은 진행중인 아이템만
 // 전체탭을 누르면 다시 전체아이템으로 돌아옴
@@ -13,8 +16,14 @@ let taskList = []
 addButton.addEventListener("click", addTask)
 
 function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    
+    let task = {
+        id : randomIDGenerate(),
+        taskContent : taskInput.value,
+        isComplete:false
+    }
+    taskList.push(task);
+    console.log(task);
     render();
 }
 
@@ -22,9 +31,9 @@ function render(){
     let resultHTML = '';
     for(let i = 0; i<taskList.length; i++){
         resultHTML += `<div class="task">
-        <div> ${taskList[i]} </div>
+        <div> ${taskList[i].taskContent} </div>
            <div>
-              <button>Check</button>
+              <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
               <button>Delete</button>
            </div>
      </div>`
@@ -32,4 +41,14 @@ function render(){
 
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+
+function toggleComplete(id){
+    console.log('id:',id)
+}
+
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
